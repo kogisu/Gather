66ad8d6bc999e7161b17cd92cb6db446a8622031
Kent Ogisu <kentogisu@gmail.com>
2018-12-01 12:39:55 -0500

    [WIP] Added hover css class to place.  Changing color not working

--- react-client/src/components/Place.jsx
+++ react-client/src/components/Place.jsx
@@ -1,28 +1,51 @@
 import React from 'react';
 import styles from '../styles/styles.css';
 
-const Place = (props) => {
-  return (
-    <a href={props.place.website ? props.place.website : props.place.url} style={{'textDecoration': 'none', color: 'black'}}>
-    <div>
-      <div className={styles.place}>
+class Place extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hover: false
+    }
+    this.handleHoverOn = this.handleHoverOn.bind(this);
+    this.handleHoverOff = this.handleHoverOff.bind(this);
+  }
+  handleHoverOn() {
+    this.setState({hover: true});
+  }
+  handleHoverOff() {
+    this.setState({hover: false});
+  }
+  render() {
+    let hover = this.state.hover ? styles.hoverOn: styles.hoverOff; 
+    return (
+      <a 
+        href={this.props.place.website ? this.props.place.website : this.props.place.url} 
+        style={{'textDecoration': 'none', color: 'black'}}
+        onMouseEnter={this.handleHoverOn}
+        onMouseLeave={this.handleHoverOff}
+        className={hover}
+      >
       <div>
-        <span>{props.place.name}</span><br/>
-        <span>{props.place.types.length <= 2 ? props.place.types.join(', ') : props.place.types.slice(0,2).join(', ') + '..'}</span>
+        <div className={styles.place + hover}>
+        <div>
+          <span>{this.props.place.name}</span><br/>
+          <span>{this.props.place.types.length <= 2 ? this.props.place.types.join(', ') : this.props.place.types.slice(0,2).join(', ') + '..'}</span>
+        </div>
+        <div>
+          <span>Rating: {this.props.place.rating}</span><br/>
+          <span>Reviews: {this.props.place.reviews}</span>
+        </div>
+        <div></div>
+        </div>
+        <div className={styles.line}>
+        <hr className={styles.placeLine}/>
+        <div />
+        </div>
       </div>
-      <div>
-        <span>Rating: {props.place.rating}</span><br/>
-        <span>Reviews: {props.place.reviews}</span>
-      </div>
-      <div></div>
-      </div>
-      <div className={styles.line}>
-      <hr className={styles.placeLine}/>
-      <div />
-      </div>
-    </div>
-    </a>
-  )
+      </a>
+    )
+  }
 }
 
 export default Place;
